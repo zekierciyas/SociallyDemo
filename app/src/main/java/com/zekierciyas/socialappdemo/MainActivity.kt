e86d77b9b3b206eab8a755b53e0d6c9765d4ae58
zekierciyas <71823127+zekierciyas@users.noreply.github.com>
2023-02-08 13:59:32 +0300

    Setup arch

---
+++ app/src/main/java/com/zekierciyas/socialappdemo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.zekierciyas.socialappdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var progressState: ProgressState = ProgressState.FirstState
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        val button = findViewById<Button>(R.id.button_next_v1)
+        val progressVar = findViewById<ImageView>(R.id.progress_bar)
+        val imageHero = findViewById<ImageView>(R.id.image_view_hero)
+
+
+        button.setOnClickListener {
+            when (progressState) {
+              is ProgressState.FirstState -> {
+                  imageHero.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_hero_img_v2))
+                  button.setBackgroundResource(R.drawable.ic_button_next_v2)
+                   progressState = ProgressState.SecondState
+                    progressVar.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_progress_bar_second))
+               }
+
+               is ProgressState.SecondState -> {
+                   button.setBackgroundResource(R.drawable.ic_button_next_v1)
+                   progressState = ProgressState.LastState
+                    progressVar.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_progress_bar_third))
+               }
+
+                is ProgressState.LastState -> {
+                    Toast.makeText(this, "Navigate time", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    sealed class ProgressState {
+        object FirstState: ProgressState()
+        object SecondState: ProgressState()
+        object LastState: ProgressState()
+    }
+}
---
+++ presentation/src/main/java/com/zekierciyas/presentation/SplashFragment.kt
@@ -0,0 +1,4 @@
+package com.zekierciyas.presentation
+
+class SplashFragment {
+}
